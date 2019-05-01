67dfa484e8c575ed454e4b5121e1eab216dea59b
Miron Aseev <aseev.miron@gmail.com>
2019-05-01 21:51:53 +0700

    Set up a Spring Boot application skeleton

---
+++ src/main/java/io/github/maseev/mapfinder/MapfinderApplication.java
@@ -0,0 +1,13 @@
+package io.github.maseev.mapfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapfinderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MapfinderApplication.class, args);
+  }
+
+}
